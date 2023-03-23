8ac750f310cf0b5cfdf24ffee2b06e5750373efe
omerUnver <muhammedomerunver@gmail.com>
2023-03-23 15:50:02 +0300

    Initial Commit

---
+++ NobetciEczaneSwiftUIApp/NobetciEczaneSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NobetciEczaneSwiftUIAppApp.swift
+//  NobetciEczaneSwiftUIApp
+//
+//  Created by M.Ömer Ünver on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NobetciEczaneSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
